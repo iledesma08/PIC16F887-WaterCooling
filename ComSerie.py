6f1cb30c948fd40064e20629f408ccc08625ce3e
Ignacio Ledesma <125617084+iledesma08@users.noreply.github.com>
2024-06-11 18:55:54 -0300

    Script de Python para comunicacion en serie
    Muestra la temperatura recibida por transmision en serie

---
+++ ComSerie.py
@@ -0,0 +1,32 @@
+import serial
+
+def read_adc_data(port, baud_rate=9600, timeout=1):
+    try:
+        # Configuración del puerto serial
+        ser = serial.Serial(port, baud_rate, timeout=timeout)
+        
+        print(f"Conectado al puerto {port} a un baud rate de {baud_rate}.")
+        
+        while True:
+            try:
+                temperature = ser.readline()
+                if temperature:
+                    # Convertimos el byte recibido a un entero, especificando el byteorder
+                    temperature = int.from_bytes(temperature, byteorder='little')
+                    print(f"Temperatura detectada: {temperature}°C")
+
+            except KeyboardInterrupt:
+                break
+
+    except serial.SerialException as e:
+        print(f"Error: {e}")
+
+    finally:
+        if 'ser' in locals() and ser.is_open:
+            ser.close()
+            print("Conexion en serie finalizada.")
+
+if __name__ == "__main__":
+    # Cambiar esto al puerto serial correcto
+    port = 'COM1'  
+    read_adc_data(port)
